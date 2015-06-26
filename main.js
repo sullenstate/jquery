69a3e81a7bf80acf0d104e60a6d705cc73f33517
Tim <tklein@outlook.com>
2015-06-25 18:19:45 -0600

    Thursday afternoon commit

--- main.js
+++ main.js
@@ -16,15 +16,35 @@
 // });
 // >>>>>>>> End Part 1 <<<<<<<<
 
+// >>>>>>>> Part 2 <<<<<<<<
+
+	// $('p').hover(function(){
+	// 	$(this).css('color', 'red');
+	// } , function(){
+	// 	$(this).css('color', 'black');
+	// });
+	// $(':header').mouseover(function(){
+	// 	if ($(this).text().substr(- 1) !== '!') {
+	// 		$(this).append('!');
+	// 	} 
+	// });
+
+	// $('a').click(function(){
+	// 	if (confirm('Do you really want to visit ' + $('a').attr('href') + '?') === false) {
+	// 		$('a').contents().unwrap();
+	// 		return false;
+	// 	} else {
+	// 		return true;
+	// 	}
+	// });
+
+// >>>>>>>> End Part 2 <<<<<<<<
 $(document).ready(function(){
-	$('p').hover(function(){
-		$(this).css('color', 'red');
-	} , function(){
-		$(this).css('color', 'black');
+	$('.main').click( function(){
+		$('.pageContainer').append("<div class='popUp'><h2>This is my popup message.</h2><div class='button second'>Make me go away!</div></div>");
 	});
-	$(':header').mouseover(function(){
-		if ($(this).text().substr(- 1) !== '!') {
-			$(this).append('!');
-		} 
+	
+	$('.pageContainer').on('click', '.popUp', function(){
+		$('.popUp').remove();
 	});
 });
